16125807056de799cb07baea3230937c2d86b016
Blake <jwsung91@gmail.com>
2026-07-27 19:38:43 +0900

    style: format Windows Asio definition

--- cmake/FindOrBuildWiresteadCore.cmake
+++ cmake/FindOrBuildWiresteadCore.cmake
@@ -120,8 +120,8 @@ if(WIRESTEAD_CORE_SOURCE_DIR)
 
   if(WIN32)
     target_compile_definitions(
-      ${WIRESTEAD_PYTHON_CORE_TARGET}
-      PUBLIC WIRESTEAD_STATIC_DEFINE BOOST_ASIO_DISABLE_IOCP
+      ${WIRESTEAD_PYTHON_CORE_TARGET} PUBLIC WIRESTEAD_STATIC_DEFINE
+                                             BOOST_ASIO_DISABLE_IOCP
     )
   endif()
 else()
